5d9dd2b8281735f3af3a839cd3057e2f2c2569d3
Shrey Dubey <wshrey09@gmail.com>
2020-07-20 23:52:22 +0530

    Added image files

--- scrape2.js
+++ scrape2.js
@@ -1,13 +1,27 @@
 const request = require('request');
 const cheerio = require('cheerio');
+const fs = require('fs');
 
 
-request('https://inc42.com/resources/open-source-for-entrepreneurs-linux/', (error,response,html) => {
-    if(!error && response.statusCode == 200){
-        const $ = cheerio.load(html);
-        const h2 = $('h2');
-        h2.map(function(i,el){
-            console.log($(el).text());
-        })
+
+fs.mkdir('./test-dir', function(err){
+    if(err){
+        console.log(error);
+    }
+    else{
+        console.log("Directory created");
     }
 })
+// request('https://wordinfo.info/results?searchString=defdsr', (error,response,html) => {
+//     if(!error && response.statusCode == 200){
+//         // console.log(html);
+//         const $ = cheerio.load(html);
+//         const selector = $('#Words > div > div.definition > center:nth-child(3) > img')
+//         if(selector.attr('src')){
+//             console.log('exists');
+//         }
+//         else{
+//             console.log('not exists');
+//         }
+//     }
+// })
